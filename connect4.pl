5ad8c3931e85271950fa1b6f6ac929dc938be653
Alexis Lessard <alexislessard95@gmail.com>
2017-03-14 23:11:58 -0400

    ajout de commentaires dans le fichier

--- connect4.pl
+++ connect4.pl
@@ -1,3 +1,5 @@
+%Go/0
+%Prédicat démarant la partie
 go :- how_to_play,
     strt(
     [[v,v,v,v,v,v],
@@ -7,13 +9,16 @@ go :- how_to_play,
     [v,v,v,v,v,v],
     [v,v,v,v,v,v],
     [v,v,v,v,v,v]]).
-
+%how_to_play/0
+%Prédicat d'affichage des règles
 how_to_play :-
   write('Vous êtes le joueur rouge (r). Vous devez aligner 4 jetons horizontalement, verticalement ou diagonalement.'),
   nl,
   write('Choisissez la colonne ou vous voulez placer votre jeton (1-7)'),
   nl.
 
+%afficheGrille/1
+%Affiche la grille au complet
 afficheGrille([C1,C2,C3,C4,C5,C6,C7]) :- 
     nth0(0,C1,V1),
     nth0(0,C2,V2),
@@ -24,7 +29,8 @@ afficheGrille([C1,C2,C3,C4,C5,C6,C7]) :-
     nth0(0,C7,V7),
     write('|'),write([V1,V2,V3,V4,V5,V6,V7]),write('|'),nl,
     afficheGrilleRec([C1,C2,C3,C4,C5,C6,C7],1).
-
+%afficheGrilleRec/2
+%Affiche la ligne de l'indice I et appel afficheGrilleRec(Grille,I+1) pour afficher la ligne suivante.
 afficheGrilleRec([C1,C2,C3,C4,C5,C6,C7],5) :- 
     nth0(5,C1,V1),
     nth0(5,C2,V2),
@@ -47,4 +53,4 @@ afficheGrilleRec([C1,C2,C3,C4,C5,C6,C7],I) :-
     I2 is I+1,
     afficheGrilleRec([C1,C2,C3,C4,C5,C6,C7],I2).
     
-strt(Grille).
+strt(Grille) :- afficheGrille(Grille).
