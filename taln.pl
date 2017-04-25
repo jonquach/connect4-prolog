a46ec9480831cccd1a127ea046f868bc84564b0b
Alexis Lessard <alexislessard95@gmail.com>
2017-04-24 21:29:44 -0400

    ajout de commentaire et de go

--- taln.pl
+++ taln.pl
@@ -1,17 +1,24 @@
+%go/0
+%prédicat de début de l'exécution de l'analyseur
 go :- readln(P),
         p(P,[]),
+        nl,
         go.
 
-
+%Phrases de forme A
 p --> [Sujet],etre,[Qualificatif], 
         {SEM =.. [Qualificatif,Sujet],write(SEM),assert(SEM)}.
 
+%Phrases de forme B
 p --> t,[Sujet],etre,[Qualificatif],
     {Consequence =..[Qualificatif,X],Condition =..[Sujet,X],assert(Consequence :- Condition),write(Consequence :- Condition)}.
 
+%Phrases de forme C
 p--> question, [Sujet],etre,[Qualificatif],
     {SEM =.. [Qualificatif,Sujet],valide(SEM)}.
 
+%valide/1
+%Valide si X est valide. Si oui, on écrit oui. Sinon, on écrit non.
 valide(X) :- X,write("oui").
 valide(X) :- not(X),write("non").
 
